a8a26ecefdf5af86fa9d4f9a8e503f96aa7042ca
LenNguyenVan13 <nguyenvanlentech@gmail.com>
2021-09-22 16:48:18 +0700

    Add Entity va chay database ok

---
+++ src/main/java/com/example/springboot_jwt_lennguyenvan/entity/Permission.java
@@ -0,0 +1,18 @@
+package com.example.springboot_jwt_lennguyenvan.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "permission")
+@Getter
+@Setter
+public class Permission extends BaseEntity{
+
+    private String permissionName;
+    private String permissionKey;
+}
