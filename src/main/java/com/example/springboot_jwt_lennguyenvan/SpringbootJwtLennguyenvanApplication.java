628aa674d90df59a52414e20822d1b42b81b93a0
LenNguyenVan13 <nguyenvanlentech@gmail.com>
2021-09-22 16:35:29 +0700

    Spring JWT first commit !

---
+++ src/main/java/com/example/springboot_jwt_lennguyenvan/SpringbootJwtLennguyenvanApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_jwt_lennguyenvan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtLennguyenvanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJwtLennguyenvanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot_jwt_lennguyenvan/SpringbootJwtLennguyenvanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_jwt_lennguyenvan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJwtLennguyenvanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
